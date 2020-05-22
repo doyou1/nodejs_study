01781bd426038f43e75407820f698de60add827d
doyou1 <tla149@daum.net>
2020-05-22 16:05:13 +0900

    웹소켓 활용 장바구니 기능

---
+++ study0522/websocket/moviebooking/app.js
@@ -0,0 +1,53 @@
+//모듈 추출
+var socketio = require('socket.io');
+var express = require('express');
+var http = require('http');
+var fs = require('fs');
+
+//변수 선언
+var seats = [
+    [1,1,0,1,1,0,0,0,0,1,1,0,1,1],
+    [1,1,0,1,1,1,1,1,1,1,1,0,1,1],
+    [1,1,0,1,1,1,1,1,1,1,1,0,1,1],
+    [1,1,0,1,1,1,1,1,1,1,1,0,1,1],
+    [1,1,0,1,1,1,1,1,1,1,1,0,1,1],
+    [1,1,0,1,1,1,1,1,1,1,1,0,1,1],
+    [1,1,0,1,1,1,1,1,1,1,1,0,1,1],
+    [1,1,0,1,1,1,1,1,1,1,1,0,1,1],
+    [1,1,0,1,1,1,1,1,1,1,1,0,1,1],
+    [1,1,0,1,1,1,1,1,1,1,1,0,1,1],
+    [1,1,0,1,1,1,1,1,1,1,1,0,1,1],
+    [1,1,0,1,1,1,1,1,1,1,1,0,1,1],
+];
+
+//웹 서버 생성
+var app = express();
+var server = http.createServer(app);
+
+app.get('/',function(reqeust,response,next){
+    fs.readFile('./HTMLPage.html',function(error, data){
+        response.send(data.toString());
+    });
+});
+
+app.get('/seats',function(request,response,next){
+    response.send(seats);
+});
+
+server.listen(20000,function(){
+    console.log('Server Running at port:20000');
+});
+
+var io = socketio.listen(server);
+io.sockets.on('connection',function(socket){
+    socket.on('reserve',function(data){
+        seats[data.y][data.x] = 2;
+        io.sockets.emit('reserve',data);
+    }) ;
+    socket.on('cancle',function(data){
+        seats[data.y][data.x] = 1;
+        io.sockets.emit('cancle',data);
+    })
+});
+
+
---
+++ study0522/websocket/realtime_shopping/app.js
@@ -0,0 +1,113 @@
+//모듈 추출
+var fs = require('fs');
+var ejs = require('ejs');
+var http = require('http');
+var express = require('express');
+
+//생성자 함수
+var counter = 0;
+function Product(name, image, price, count){
+    this.index = counter++;
+    this.name = name;
+    this.image = image;
+    this.price = price;
+    this.count = count;
+}
+
+//변수 선언
+var products = [
+    new Product('JavaScript', 'chrome.png', 28000, 30),
+    new Product('jQuery', 'chrome.png', 28000, 30),
+    new Product('Node.js', 'chrome.png', 32000, 30),
+    new Product('Socket.io', 'chrome.png', 17000, 30),
+    new Product('Connect', 'chrome.png', 18000, 30),
+    new Product('Express', 'chrome.png', 31000, 30),
+    new Product('EJS', 'chrome.png', 12000, 30)
+];
+
+//웹 서버 생성
+var app =express();
+var server = http.createServer(app);
+
+//웹 서버 설정
+app.use(express.static(__dirname + '/public'));
+
+//라우트 수행
+app.get('/',function(request,response){
+    
+    var htmlPage = fs.readFileSync('./HTMLPage.html', 'utf8');
+    
+    //응답
+    response.send(ejs.render(htmlPage,{
+            products : products
+    }));
+    
+});
+
+server.listen(20000,function(){
+    console.log('Server Running port:20000');
+});
+
+var io = require('socket.io').listen(server);
+io.sockets.on('connection',function(socket){
+    //함수 선언
+    function onReturn(index){
+        //물건 개수 증가
+        products[index].count++;
+
+        //타이머 제거
+        clearTimeout(cart[index].timerID);
+
+        //카트에서 물건을 제거함
+        delete cart[index];
+
+        //count 이벤트 발생
+        io.sockets.emit('count',{
+            index : index,
+            count : products[index].count
+        });
+    }
+
+    //변수 선언
+    var cart = {};
+
+    //cart 이벤트
+    socket.on('cart',function(index){
+        //물건 개수 감소
+        products[index].count--;
+        
+        //카트에 물건 넣고 타이머 시작
+        cart[index] = {};
+        cart[index].index = index;
+        cart[index].timerID = setTimeout(function(){
+            onReturn(index);
+        }, 10 * 60 * 1000);
+
+        //count 이벤트 발생
+        io.sockets.emit('count',{
+            index : index,
+            count : products[index].count
+        });
+    });
+
+    //but 이벤트
+    socket.on('buy',function(index){
+        //타이머 제거
+        clearTimeout(cart[index].timerID);
+
+        //카트 물건 제거
+        delete cart[index];
+
+        //count 이벤트 발생
+        io.sockets.emit('count',{
+            index : index,
+            count : products[index].count
+        });
+    });
+
+    //return 이벤트
+    socket.on('return',function(index){
+        onReturn(index);
+    });
+})
+
